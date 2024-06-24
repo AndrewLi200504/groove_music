655478eb29843ae0474b5d6e3bc23407b6bc9ee8
Gary Hilares <dev.garyhilares@gmail.com>
2024-06-24 16:55:55 -0500

    Added notes

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,32 @@ function App() {
         }
       />
       <NoteAdder
-        notes={["C4", "C#4", "D4"]}
+        notes={[
+          "C3",
+          "C#3",
+          "D3",
+          "D#3",
+          "E3",
+          "F3",
+          "F#3",
+          "G3",
+          "G#3",
+          "A3",
+          "A#3",
+          "B3",
+          "C4",
+          "C#4",
+          "D4",
+          "D#4",
+          "E4",
+          "F4",
+          "F#4",
+          "G4",
+          "G#4",
+          "A4",
+          "A#4",
+          "B4",
+        ]}
         addNote={(note) =>
           setComposition((oldComposition) => [...oldComposition, note])
         }
