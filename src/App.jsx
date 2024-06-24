13766ec5436b58fc5fb698541c745b9c2c5f2e75
Gary Hilares <dev.garyhilares@gmail.com>
2024-06-24 16:52:54 -0500

    Added basic play function

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
         toSave={composition}
         onLoad={setComposition}
         onExport={logArguments}
-        onPlay={logArguments}
       />
       <TrackManager
         composition={composition}
