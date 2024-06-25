6398152d1a689dee33784298e26276c64b4290f3
Andrew Li <andrewli200504@gmail.com>
2024-06-25 13:46:04 -0700

    Added spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ClipLoader from 'react-spinners/ClipLoader';
+const override = {
+    display: 'block',
+    margin: '100px auto'
+}
+const Spinner = ({ loading }) => {
+  return (
+    <ClipLoader
+    color='#4338ca'
+    loading={loading}
+    cssOverride={override}
+    size={150} />
+  )
+}
+
+export default Spinner
