ca3cde7c13da5d3e9c542d86731344c0fc43a125
Gary Hilares <dev.garyhilares@gmail.com>
2024-06-25 09:53:12 -0500

    Moved notes from rendering to a constant

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,32 @@ function logArguments() {
 
 function App() {
   const [composition, setComposition] = useState([]);
+  const notes = [
+    "C3",
+    "C#3",
+    "D3",
+    "D#3",
+    "E3",
+    "F3",
+    "F#3",
+    "G3",
+    "G#3",
+    "A3",
+    "A#3",
+    "B3",
+    "C4",
+    "C#4",
+    "D4",
+    "D#4",
+    "E4",
+    "F4",
+    "F#4",
+    "G4",
+    "G#4",
+    "A4",
+    "A#4",
+    "B4",
+  ];
   return (
     <>
       <ButtonsBar
@@ -32,32 +58,7 @@ function App() {
         }
       />
       <NoteAdder
-        notes={[
-          "C3",
-          "C#3",
-          "D3",
-          "D#3",
-          "E3",
-          "F3",
-          "F#3",
-          "G3",
-          "G#3",
-          "A3",
-          "A#3",
-          "B3",
-          "C4",
-          "C#4",
-          "D4",
-          "D#4",
-          "E4",
-          "F4",
-          "F#4",
-          "G4",
-          "G#4",
-          "A4",
-          "A#4",
-          "B4",
-        ]}
+        notes={notes}
         addNote={(note) =>
           setComposition((oldComposition) => [...oldComposition, note])
         }
