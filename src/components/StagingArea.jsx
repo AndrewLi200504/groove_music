7bd317a1a246cc413be720674f3278e91ba13f21
Gary Hilares <dev.garyhilares@gmail.com>
2024-07-10 14:54:07 -0500

    Changed styles

--- src/components/StagingArea.jsx
+++ src/components/StagingArea.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const StagingArea = () => {
   return (
-    <div className="inline-block box-border m-auto h-[100%] w-[80%] p-4 
-    border-4 bg-green-500 m4">
-        E
+    <div
+      className="inline-block box-border m-auto h-[100%] w-[20%] p-4 
+    border-4 bg-green-500 m4"
+    >
+      E
     </div>
-  )
-}
+  );
+};
 
-export {StagingArea};
+export { StagingArea };
