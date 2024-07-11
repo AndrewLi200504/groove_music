6afc77fb471f800198cc4728da67121a5d55f71a
Andrew Li <andrewli200504@gmail.com>
2024-07-10 18:12:48 -0700

    change height of staging

--- src/components/StagingArea.jsx
+++ src/components/StagingArea.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const StagingArea = () => {
   return (
-    <div className="inline-block box-border m-auto h-[100%] w-[80%] p-4 
+    <div className="inline-block box-border m-auto h-100vh w-[20%] p-4 
     border-4 bg-green-500 m4">
         E
     </div>
