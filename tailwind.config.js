c2f6cdaa82c7b62e2c4a8332d5dce5ae4315d9fc
Gary Hilares <dev.garyhilares@gmail.com>
2024-07-10 19:54:51 -0500

    Setup Tailwindcss live reload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
