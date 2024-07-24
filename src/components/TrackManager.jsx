c1f8ddf4dbd0fb48ae71c56b4ff41ee8fce990e7
Gary Hilares <dev.garyhilares@gmail.com>
2024-07-23 21:52:09 -0500

    Added animation while adding a new note

--- src/components/TrackManager.jsx
+++ src/components/TrackManager.jsx
@@ -2,39 +2,54 @@ import React, { useState } from "react";
 
 const TEMPO_WIDTH = 40;
 
-function TrackNote({ note, deleteNote }) {
+function TrackNote({ position, duration, ...props }) {
   return (
     <div
       className="absolute border-black bg-green-500 h-full"
       style={{
         top: 0,
-        left: note.position * TEMPO_WIDTH,
-        width: note.duration * TEMPO_WIDTH,
+        left: position * TEMPO_WIDTH,
+        width: duration * TEMPO_WIDTH,
       }}
-      onClick={() => deleteNote(note)}
+      {...props}
     />
   );
 }
 
 function TrackRow({ tone, addNote, deleteNote, notes }) {
-  const [lastMouseDown, setLastMouseDown] = useState(null);
+  const [mouseInfo, setMouseInfo] = useState(null);
   const minWidth =
     (Math.max(...notes.map((note) => note.position + note.duration), 0) + 5) *
     TEMPO_WIDTH;
   function handleMouseDown(event) {
     if (event.button == 0) {
-      setLastMouseDown(event.nativeEvent.offsetX / TEMPO_WIDTH);
+      setMouseInfo({
+        last: event.nativeEvent.offsetX / TEMPO_WIDTH,
+        now: event.nativeEvent.offsetX / TEMPO_WIDTH,
+      });
     }
   }
   function handleMouseUp(event) {
     const end = event.nativeEvent.offsetX / TEMPO_WIDTH;
-    if (event.button == 0 && lastMouseDown != null) {
-      const duration = end - lastMouseDown;
+    if (event.button == 0 && mouseInfo) {
+      const duration = end - mouseInfo.last;
       if (duration > 0.1) {
-        addNote({ tone: tone, position: lastMouseDown, duration: duration });
+        addNote({ tone: tone, position: mouseInfo.last, duration: duration });
       }
-      setLastMouseDown(null);
+      setMouseInfo(null);
     }
+  }
+  function handleMouseMove(event) {
+    if (mouseInfo) {
+      const mouseX = event.nativeEvent.offsetX / TEMPO_WIDTH;
+      setMouseInfo((oldInfo) => ({
+        ...oldInfo,
+        now: mouseX,
+      }));
+    }
+  }
+  function handleMouseLeave() {
+    setMouseInfo(null);
   }
   return (
     <div className="flex">
@@ -44,11 +59,24 @@ function TrackRow({ tone, addNote, deleteNote, notes }) {
       <div
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
         className="inline-block relative box-border border-2 border-black bg-white-500 h-10 grow"
         style={{ minWidth: minWidth }}
       >
+        {mouseInfo && mouseInfo.now > mouseInfo.last && (
+          <TrackNote
+            position={mouseInfo.last}
+            duration={mouseInfo.now - mouseInfo.last}
+          />
+        )}
         {notes.map((note) => (
-          <TrackNote key={note.position} note={note} deleteNote={deleteNote} />
+          <TrackNote
+            key={note.position}
+            position={note.position}
+            duration={note.duration}
+            onClick={() => deleteNote(note)}
+          />
         ))}
       </div>
     </div>
